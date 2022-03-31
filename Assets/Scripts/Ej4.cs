c15c2170c755ee969b0cd37c950600db74455bc3
feliann <101577329+feliann@users.noreply.github.com>
2022-03-31 09:03:19 -0300

    exercise 4 finished

--- Assets/Scripts/Ej4.cs
+++ Assets/Scripts/Ej4.cs
@@ -8,9 +8,14 @@ public class Ej4 : MonoBehaviour
     //Luego, mostrar el resultado de la suma entre ambos al ejecutar la escena utilizando el siguiente formato:
     //“La  suma entre “ + num1 + “ y “ + num2 + “ da como resultado “ + num1 + num2. 
 
+    public int num1;
+    public int num2;
     void Start()
     {
-        
+        int suma = num1 + num2;
+
+        Debug.Log("La  suma entre " + num1 + " y " + num2 + " da como resultado " + suma);
+
     }
 
     // Update is called once per frame
