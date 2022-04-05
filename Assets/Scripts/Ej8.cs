f0c96f53ef7316cbb65c688f6c849eb982c675d3
feliann <101577329+feliann@users.noreply.github.com>
2022-04-05 10:37:10 -0300

    Finished day hasta el Ej7

---
+++ Assets/Scripts/Ej8.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej8 : MonoBehaviour
+//    8. Crear una variable entera que pueda ser modificada desde el Inspector.Suponiendo que
+//el primer día de la semana es el Domingo, mostrar el nombre del día correspondiente al
+//valor de la variable “dia”. Si el día no está en el rango permitido debe emitir por pantalla el
+//mensaje de error &quot;El día ingresado no es válido&quot;.
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
